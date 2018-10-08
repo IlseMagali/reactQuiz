5833d6385ecdda1c9e7b0944c90dd42305b67b47
ilsekeymolen <ikeymolen@itexico.net>
2018-10-08 17:10:10 -0500

    adding different titles random to get part of the quiz ready to start with events

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import "./App.css";
+import "./bootstrap.min.css";
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1> Author Quiz </h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  );
+}
+
+function Book({ title }) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+   </div>
+  );
+}
+
+function Turn({author}) {
+  return (
+    <div className="row turn" style={{ backgroundColor: "white" }}>
+      <div className="col-4 offset-1">
+        {/* <img src={author} className="authorimage" alt="Author" /> */}
+        <p>{console.log(author)}</p>
+      </div>
+      <div className="col-6">
+        {author.books.map(title => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return <button />;
+}
+
+function Footer() {
+  return (
+    <div className="row">
+      <p className="text-muted credit">
+        All images are from{" "}
+        <a href="https://commons.wikimedia.org/wiki/Main_Page">
+          Wikimedia Commons
+        </a>
+      </p>
+    </div>
+  );
+}
+
+function AuthorQuiz({ turnData }) {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Turn {...turnData} className="turn" />
+      <Continue />
+      <Footer />
+    </div>
+  );
+}
+
+export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Defining application data
+const authors = [
+  {
+    name: 'William Shakepeare',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: [
+      'Othello',
+      'Romeo and Juliet',
+      'Hamlet',
+      'The Tempest',
+      'Macbeth'
+    ]
+  },
+  {
+    name: 'Rosario Castellanos',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: [
+      'Sobre cultura femenina',
+      'Mujer que sabe latín',
+      'Rito de iniciación',
+      'El eterno femenino',
+      'Cartas a Ricardo'
+    ]
+  },
+  {
+    name: 'Jorge Volpi',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: [
+      'Las guerras y las palabras',
+      'Leer la mente. El cerebro y el arte de la ficción',
+      'La paz de los sepulcros',
+      'Memorial del engaño',
+      'In search of Klingsor'
+    ]
+  },
+  {
+    name: 'Ángeles Mastretta',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: [
+      'Arráncame la vida',
+      'Mujeres de ojos grandes',
+      'Ninguna eternidad como la mía',
+      'Puerto Libre',
+      'El mundo iluminado'
+    ]
+  },
+  {
+    name: 'Agatha Christie',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: [
+      'The Murder of Roger Ackroyd',
+      'The Moving Finger',
+      'Crooker House',
+      'Ordeal by Innocence',
+      'Murder on the Orient Express'
+    ]
+  }
+];
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books)
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) => author.books.some((title) => title === answer))
+  }
+}
+
+const state = {
+  turnData: {
+    author: getTurnData(authors)
+  }
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
